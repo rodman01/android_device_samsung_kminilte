864f4e399323943bbc4fd0b25390c8f6fb4d8526
rodman01 <email4joern@arcor.de>
2018-11-05 20:48:25 +0100

    Revert "Revert "move to klte nfc paths and sources 1/2""
    This reverts commit ea73475ca5f2858b904391af107bdfb7f89accd0.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,8 +57,11 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 # NFC
 # Chipset: Samsung s3fwrn5
 # Note: as libnfc-nci only supports pn547 and bcm2079x, select pn547 here but use the stock s3fwrn5 hal
-BOARD_NFC_CHIPSET := pn547
-BOARD_NFC_HAL_SUFFIX := universal3470
+# BOARD_NFC_CHIPSET := pn547
+# BOARD_NFC_HAL_SUFFIX := universal3470
+
+# NFC
+include device/samsung/klte-common/nfc/pn547/board.mk
 
 # PowerHAL
 TARGET_POWERHAL_VARIANT := exynos3
